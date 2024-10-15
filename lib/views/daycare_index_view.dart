296beaacd78f6805e31d7633c517b468844b6e72
Cleidson <cleidson.mac@outlook.com>
2024-10-15 08:59:29 +0100

    Basic Skeleton Code part 1

---
+++ lib/views/daycare_index_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DaycareIndexView extends StatelessWidget {
+  const DaycareIndexView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          child: Text('Daycare Home Index Page!'),
+        ),
+    );
+  }
+}
