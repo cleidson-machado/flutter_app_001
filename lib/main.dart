17c7f0f91581980cdb3df8e45872355896d03e5b
Cleidson <cleidson.mac@outlook.com>
2024-10-24 16:44:47 +0100

    Step 001 To Recreate Default Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,61 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import 'package:my_view_app_001/views/daycare_client_view.dart';
-// import 'package:my_view_app_001/views/daycare_index.dart';
-// import 'package:my_view_app_001/views/testcode/list_view_text_example.dart';
-import 'package:my_view_app_001/views/testcode/list_view_text_example2.dart';
-// import 'package:my_view_app_001/views/testcode/check_box_change_button.dart';
+import 'package:my_view_app_001/Components/colours_library.dart';
 
-void main() {
-  runApp(const MyDaycareApp());
-}
+/// Flutter code sample for [CupertinoPageScaffold].
+/// MyAppLoginLegacyView
 
-class MyDaycareApp extends StatelessWidget {
-  const MyDaycareApp({super.key});
+void main() => runApp(const MyAppLoginLegacyView());
 
-  
+class MyAppLoginLegacyView extends StatelessWidget {
+  const MyAppLoginLegacyView({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyListExample2()
-      // home: const DayCareClientView()
-      // home: const DaycareIndexView(),
-      // home: const CheckBoxActionExampleApp(),
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: PageScaffoldExample(),
     );
   }
 }
 
+class PageScaffoldExample extends StatefulWidget {
+  const PageScaffoldExample({super.key});
 
+  @override
+  State<PageScaffoldExample> createState() => _PageScaffoldExampleState();
+}
 
+class _PageScaffoldExampleState extends State<PageScaffoldExample> {
+  int _count = 0;
 
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'ISS - Instituto da Segurança Social';
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.white,
+      navigationBar: const CupertinoNavigationBar(
+        backgroundColor: defaultGreenButtonColor,
+        middle: Text(appTitle, 
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white70)),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text('You have pressed the button $_count times.'),
+            ),
+            const SizedBox(height: 20.0),
+            Center(
+              child: CupertinoButton.filled(
+                onPressed: () => setState(() => _count++),
+                child: const Icon(CupertinoIcons.add),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
