f4955f362c8dfe1358fdc44bedd367e6af16606e
Cleidson <cleidson.mac@outlook.com>
2024-11-02 13:15:44 +0000

    Adding Basic Menu Icons and Base Code for the Menu

--- lib/Components/nav_bars/nav_bar_index.dart
+++ lib/Components/nav_bars/nav_bar_index.dart
@@ -9,14 +9,62 @@ class NavBar extends StatelessWidget {
     return Drawer(
       child: ListView(
         padding: EdgeInsets.zero,
-        children: const [
-          UserAccountsDrawerHeader(
-            accountName: Text('CLEIDSON PEREIRA MACHADO', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
-            accountEmail: Text('N° Segurança Social: 1227.6956.698'),
-            decoration: BoxDecoration(
-              color: defaultGreenButtonColor,
+        children: [
+          const UserAccountsDrawerHeader(
+              accountName: Text('CLEIDSON PEREIRA MACHADO', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: defaultGreenButtonColor)),
+              accountEmail: Text('NISS: 1227.6956.698', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black54)),
+              decoration: BoxDecoration(
+                color: defaultGreenButtonColor,
+                image: DecorationImage(image: AssetImage('assets/images/MenuAppPhotoFnd.png'),
+                fit: BoxFit.cover,)
+              ),
             ),
-            )
+          ListTile(
+            leading: const Icon(Icons.person_search_rounded),
+            title: const Text("Posição Atual", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          ListTile(
+            leading: const Icon(Icons.wallet_rounded),
+            title: const Text("Documentos de Pagamento", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          ListTile(
+            leading: const Icon(Icons.card_membership_rounded),
+            title: const Text("Cartão Europeu de Seguro", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          ListTile(
+            leading: const Icon(Icons.inbox_rounded),
+            title: const Text("Menssagens", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          ListTile(
+            leading: const Icon(Icons.calendar_month_rounded),
+            title: const Text("Agenda", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings_rounded),
+            title: const Text("Definições", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          ListTile(
+            leading: const Icon(Icons.perm_phone_msg_rounded),
+            title: const Text("Contactos", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          ListTile(
+            leading: const Icon(Icons.live_help_rounded),
+            title: const Text("Informações", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
+          const SizedBox(height: 66),
+          ListTile(
+            leading: const Icon(Icons.logout_rounded),
+            title: const Text("SAIR", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.black)),
+            onTap: () => {null},
+          ),
         ],
       ),
     );
