ae0e6187472f62da3cf0c232722fb83825671a94
Cleidson <cleidson.mac@outlook.com>
2024-11-20 18:34:22 +0000

    Add First Basic Page

---
+++ lib/presentation/home/vacancy_home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VacancyHomePage extends StatelessWidget {
+  const VacancyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(),
+    );
+  }
+}
