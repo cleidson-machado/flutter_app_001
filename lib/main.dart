dbefce497cf1b8f93aed615bdfea2d320717f0f6
Cleidson <cleidson.mac@outlook.com>
2024-11-21 17:29:14 +0000

    Add Phoenix Dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:my_view_app_001/app/app_module.dart';
 import 'package:my_view_app_001/app/app_widget.dart';
 
 void main() {
-  runApp(ModularApp(
-    module: AppModule(),
-    child:  const AppWidget(),
-  ));
+  runApp(Phoenix(
+    child: ModularApp(
+      module: AppModule(), 
+      child: const AppWidget())));
 }
 
