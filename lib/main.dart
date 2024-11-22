ffa61499a5439793ae17aa5bd3ae2b52b6fade59
Cleidson <cleidson.mac@outlook.com>
2024-11-22 15:06:32 +0000

    Just Remove Phoenix From The main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:flutter_phoenix/flutter_phoenix.dart';
+//import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:my_view_app_001/app/app_module.dart';
 import 'package:my_view_app_001/app/app_widget.dart';
 
 void main() {
-  runApp(Phoenix(
-    child: ModularApp(
+  runApp(ModularApp(
       module: AppModule(), 
-      child: const AppWidget())));
+      child: const AppWidget()));
 }
 
