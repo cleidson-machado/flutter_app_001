297e6fa7ee50294cf9604e98425017f9342eedd0
Cleidson <cleidson.mac@outlook.com>
2024-11-22 14:49:13 +0000

    Fix The Test Class

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:my_view_app_001/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const VagaAppModule());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
