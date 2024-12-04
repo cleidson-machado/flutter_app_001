1c6844aa48e0ffbc7d287e4c43d2e7f6dd528e73
Cleidson <cleidson.mac@outlook.com>
2024-12-04 18:41:59 +0000

    Creation of the First Code Layout for The Search Form Page

--- lib/app/feature/b_search_start_form/search_start_form_view.dart
+++ lib/app/feature/b_search_start_form/search_start_form_view.dart
@@ -11,7 +11,7 @@ class _SearchStartFormViewState extends State<SearchStartFormView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
           backgroundColor: Colors.green,
           title: const Text('Cooperação',
               style:
@@ -19,38 +19,237 @@ class _SearchStartFormViewState extends State<SearchStartFormView> {
           iconTheme: const IconThemeData(color: Colors.white),
         ),
         body: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: ListView(
-              children: <Widget>[
-                GestureDetector(
-                  onTap: () {},
-                  child: Card(
-                    elevation: 0.5,
-                    shadowColor: Colors.black,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0),
-                        side: const BorderSide(color: Colors.grey, width: 0.5)),
-                    color: Colors.white,
-                    child: const Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              '-- Search Form --',
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  fontStyle: FontStyle.normal,
-                                  fontWeight: FontWeight.normal,
-                                  color: Colors.black87),
-                            ),
-                          ],
-                        )),
+          color: Colors.lightBlue[100], //MARCA!! ROOT Widget
+          child: Column(
+            children: [
+              // CABECAÇHO! #############################
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'Pesquisa de vagas',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
                   ),
                 ),
-              ],
-            ))
-    );
+              ),
+
+              //############################# COMBO 1
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'Combo 1 - Resposta Social',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+              //############################# COMBO 2
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'Combo 2 - Zona de Residência',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+              //############################# COMBO 3
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'Combo 3 - Distrito',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+              //############################# COMBO 4
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'Combo 4 - Conselho',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+              //############################# COMBO 5
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'Combo 5 - Freguesia',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+              //############################# TXT de Observação
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'TXT de Observação',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+              //############################# Radio BTN
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'radio BTN',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+              //############################# BTN de Pesquisar
+              Padding(
+                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 15),
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.grey,
+                              border: Border(bottom: BorderSide(color: Colors.black))),
+                  child: const Row(
+                    children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0, vertical: 3),
+                      child: Text(
+                        'SPACE BTN DE Pesquisar',
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black87),
+                      ),
+                    ),
+                  ],
+                  ),
+                ),
+              ),
+
+            ],
+          ) ,
+        )
+        );
   }
 }
